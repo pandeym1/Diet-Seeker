6f2bdd55a2b97b42bec7ee7ef53bffffc5e93b84
pandeym1 <73703589+pandeym1@users.noreply.github.com>
2021-12-04 11:54:45 -0600

    Editing ADJ List code
    Error with insertion function

--- Graph.h
+++ Graph.h
@@ -4,59 +4,43 @@
 #include<vector>
 using namespace std;
 
+struct node {
+    string name;
+};
+
 class Graph
 {
 private:
-    map<int, vector<pair<int, int>>> adjList;
+    map<node, vector<pair<node, int>>> adjList;
     // Graph ADJACENCY LIST data structure here 
 public:
-    void insertEdge(int from, int to, int weight);
-    bool isEdge(int from, int to);
-    int getWeight(int from, int to);
+    void insertEdge(node from, node to, int weight);
+    //bool isEdge(node from, node to);
+    //int getWeight(node from, node to);
 };
 
-void Graph::insertEdge(int from, int to, int weight)
+void Graph::insertEdge(node from, node to, int weight)
 {
-    pair<int, int> endMe;
+    pair<node, int> endMe;
     endMe.first = to;
     endMe.second = weight;
     adjList[from].push_back(endMe);
-    /*
-         TODO: insertEdge() adds a new edge between the from and to vertex.
-         You will not know how many vertices there may be.
-         insertEdge does not output or print anything
-    */
 }
 
-bool Graph::isEdge(int from, int to)
-{
-    for (auto it : adjList) {
-        if (it.first == from) {
-            for (int i = 0; i < it.second.size(); i++) {
-                if (it.second.at(i).first == to) {
-                    return true;
-                }
-            }
-        }
-    }
-    /*
-        TODO: isEdge() returns a boolean indicating true 
-        if there is an edge between the from and to vertex
-    */
-    return false;
-}
-
-int Graph::getWeight(int from, int to)
-{
-    for (auto it : adjList) {
-        if (it.first == from) {
-            for (int i = 0; i < it.second.size(); i++) {
-                if (it.second.at(i).first == to) {
-                    return it.second.at(i).second;
-                }
-            }
-        }
-    }
-    return 0;
-}
-
+//bool Graph::isEdge(node from, node to)
+//{
+//    for (auto it : adjList) {
+//        if (it.first.name == from.name) {
+//            for (int i = 0; i < it.second.size(); i++) {
+//                if (it.second.at(i).first.name == to.name) {
+//                    return true;
+//                }
+//            }
+//        }
+//    }
+//    /*
+//        TODO: isEdge() returns a boolean indicating true 
+//        if there is an edge between the from and to vertex
+//    */
+//    return false;
+//}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "Graph.h"
 using namespace std;
 
 int main() {
+	node temp;
+	temp.name = "Wroks";
+
+	cout << temp.name << endl;
 	cout << "Hello World" << endl;
 	cout << "Testing push" << endl;
 }
