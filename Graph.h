3f8eb769ac80803bbc7b2110d7d82da25d3998e7
pandeym1 <73703589+pandeym1@users.noreply.github.com>
2021-12-07 12:50:01 -0600

    Added a curr macros for users

--- Graph.h
+++ Graph.h
@@ -27,21 +27,3 @@ public:
     //bool isEdge(node from, node to);
     //int getWeight(node from, node to);
 };
-
-//bool Graph::isEdge(node from, node to)
-//{
-//    for (auto it : adjList) {
-//        if (it.first.name == from.name) {
-//            for (int i = 0; i < it.second.size(); i++) {
-//                if (it.second.at(i).first.name == to.name) {
-//                    return true;
-//                }
-//            }
-//        }
-//    }
-//    /*
-//        TODO: isEdge() returns a boolean indicating true 
-//        if there is an edge between the from and to vertex
-//    */
-//    return false;
-//}
--- User.h
+++ User.h
@@ -5,6 +5,9 @@ private:
 	float maxCarbs;
 	float maxProt;
 	float maxFat;
+	float currCarb;
+	float currProt;
+	float currFat;
 	int age;
 	int activityLevel;
 
@@ -22,6 +25,9 @@ public:
 		this->weight = WEIGHT;
 		this->height = HEIGHT;
 		this->BMR = calculateBMR(this->age, this->activityLevel, this->weight, this->height);
+		this->currCarb = 0.0f;
+		this->currProt = 0.0f;
+		this->currFat = 0.0f;
 
 		this->maxCarbs = (this->BMR) * 0.4f;
 		this->maxProt = (this->BMR) * 0.4f;
