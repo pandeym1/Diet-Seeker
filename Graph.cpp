7c44c108630fb5c0023f966217ae6ddbf446acd6
pandeym1 <73703589+pandeym1@users.noreply.github.com>
2021-12-09 11:18:19 -0600

    Finished all Graph algos

--- Graph.cpp
+++ Graph.cpp
@@ -1,5 +1,6 @@
 #include "Graph.h"
 #include <queue>
+#include <stack>
 
 void Graph::insertEdge(node from, node to, int weight) {
 	nodeStore[from.ID] = from;
@@ -57,10 +58,62 @@ vector<int> Graph::BFSsearch(string Uinput) {
 
 vector<int> Graph::DFSrec(int ID, int ID2) {
 	vector<int> recRes;
+	unordered_map<int, bool> UMV;
+	UMV[ID] = true;
+	stack<int> nodeS;
+	nodeS.push(ID);
+
+	while (!nodeS.empty()) {
+		int temp = nodeS.top();
+		nodeS.pop();
+
+		for (int i = 0; i < adjList[temp].size(); i++) {
+			if (adjList[temp].at(i).first == ID2) {
+				return recRes;
+			}
+			if (UMV.find(adjList[temp].at(i).first) != UMV.end()) {
+				UMV[adjList[temp].at(i).first] = true;
+				nodeS.push(adjList[temp].at(i).first);
+				recRes.push_back(adjList[temp].at(i).first);
+			}
+		}
+	}
 	return recRes;
 }
 
 vector<int> Graph::DijkstraRec(int ID, int ID2) {
 	vector<int> recRes;
+	unordered_map<int, bool> UMV;
+	UMV[ID] = true;
+	stack<int> nodeS;
+	nodeS.push(ID);
+
+	while (!nodeS.empty()) {
+		int temp = nodeS.top();
+		nodeS.pop();
+		int max = 0;
+		int maxID = 0;
+
+		for (int i = 0; i < adjList[temp].size(); i++) {
+			if (adjList[temp].at(i).second > max && UMV.find(adjList[temp].at(i).first) != UMV.end()) {
+				max = adjList[temp].at(i).second;
+				maxID = adjList[temp].at(i).first;
+			}
+			//if (adjList[temp].at(i).first == ID2) {
+			//	return recRes;
+			//}
+			//if (UMV.find(adjList[temp].at(i).first) != UMV.end()) {
+			//	UMV[adjList[temp].at(i).first] = true;
+			//	nodeS.push(adjList[temp].at(i).first);
+			//	recRes.push_back(adjList[temp].at(i).first);
+			//}
+		}
+
+		if (maxID == ID2) {
+			return recRes;
+		}
+		nodeS.push(maxID);
+		recRes.push_back(maxID);
+	}
 	return recRes;
 }
